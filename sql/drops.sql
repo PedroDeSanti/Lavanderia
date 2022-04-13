3d05b47e161c2c566309b636f4786acfdf7c7b68
PedroDeSanti <phmartinsanti@gmail.com>
2022-04-13 00:38:17 -0300

    ✨ Add SQL files
    Co-authored-by: vinicius-CL <vinicius-CL@users.noreply.github.com>
    Co-authored-by: Gabriel Zambelli <GabZamba@users.noreply.github.com>

---
+++ sql/drops.sql
@@ -0,0 +1,6 @@
+drop table cliente;
+drop table funcionario;
+drop table administra;
+drop table pedido;
+drop table item;
+drop table TipoLavagem;
---
+++ sql/tabAdministra.sql
@@ -0,0 +1,4 @@
+create table Administra
+	(ID_Pedido int(5),
+     CPF int(11),
+     primary key (ID_Pedido, CPF));
